21ff27c7327db626dc7e8eedce573aafcec4b42f
MartinYe1234 <martinye022@gmail.com>
2021-07-16 21:21:30 -0700

    updated how photos worked

--- src/components/PhotoUpload.vue
+++ src/components/PhotoUpload.vue
@@ -3,27 +3,36 @@
     <table>
         <thead>
             <th class="table-header">Number</th>
+            <th class="table-header">Remove</th>
         </thead>
         <tbody>
-            <tr><single-photo></single-photo></tr>
-            <tr>{{name}}</tr>
+            <tr v-for="photo in uploadedPhotos" :key="photo">
+            <td>{{photo}}</td>
+            <td><button v-on:click="removePhotoFromList(photo)">Remove</button></td>
+            </tr>
         </tbody>
     </table>
     <button v-on:click="submitPhotos"> click me</button>
 </template>
 
 <script>
-import SinglePhoto from './SinglePhoto.vue'
 export default {
-    components: { SinglePhoto },
     methods: {
         submitPhotos: function() {
-            console.log("submit endpoint")
+            console.log("submitted photos")
+        },
+        removePhotoFromList: function(photo) {
+            this.uploadedPhotos.splice(photo, 1)
+        },
+        addPhotoToList: function(photo) {
+            var insertion_index = this.uploadedPhotos.length;
+            this.uploadedPhotos.push(photo)
         }
     },
     data() {
         return {
-            name: "Martin"
+            name: "Martin",
+            uploadedPhotos: [0,1,2,3,4,5,6]
         }
     }
 }
