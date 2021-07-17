004e87444b2f3f04a00bab52477d2c4a790dd571
MartinYe1234 <martinye022@gmail.com>
2021-07-16 20:27:42 -0700

    photoupload

--- src/components/PhotoUpload.vue
+++ src/components/PhotoUpload.vue
@@ -1,13 +1,36 @@
 <template>
-    <div>upload photos</div>
+    <header>UPLOAD PHOTOS</header>
+    <table>
+        <thead>
+            <th class="table-header">Number</th>
+        </thead>
+        <tbody>
+            <tr><single-photo></single-photo></tr>
+            <tr>{{name}}</tr>
+        </tbody>
+    </table>
+    <button v-on:click="submitPhotos"> click me</button>
 </template>
 
 <script>
+import SinglePhoto from './SinglePhoto.vue'
 export default {
-
+    components: { SinglePhoto },
+    methods: {
+        submitPhotos: function() {
+            console.log("submit endpoint")
+        }
+    },
+    data() {
+        return {
+            name: "Martin"
+        }
+    }
 }
 </script>
 
 <style>
-
+.table-header{
+    background-color: red;
+}
 </style>
--- src/components/SinglePhoto.vue
+++ src/components/SinglePhoto.vue
@@ -1,11 +1,13 @@
 <template>
+  <div>photo here</div>
 </template>
 
 <script>
 export default {
+    name: 'single-photo-entry'
 }
 </script>
-  
+
 <style>
 
 </style>
