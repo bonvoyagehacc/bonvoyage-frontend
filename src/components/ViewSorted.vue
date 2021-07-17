d738cd653b3b6734a90bb35e32e8cf26fa1265de
kugelblitz2 <48634762+kugelblitz2@users.noreply.github.com>
2021-07-17 02:16:16 -0400

    Now displays photos

--- src/components/ViewSorted.vue
+++ src/components/ViewSorted.vue
@@ -1,23 +1,38 @@
 <template>
   <div class="header">
-    <img class="logo" src="../assets/logo.png" />
+    <img class="logo" src="src/assets/logo.png" />
     やなぎ
   </div>
-  <div class="grid"></div>
+  <div class="grid">
+    <div v-for="item in displaycontent" :key="item" class="displayitem">
+      <img :src="item"/>
+    </div>
+  </div>
 
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      displaycontent: ["src/assets/temp1.png", "src/assets/temp2.png", "src/assets/temp3.png"]
+    }
+  }
 }
 </script>
 
 <style>
-  .header {
-    background-color: coral;
-  }
-  .logo {
-    height: 50px;
-  }
+.header {
+  background-color: coral;
+}
+.logo {
+  height: 50px;
+}
+.grid {
+  display: flex;
+  flex-direction: row;
+}
+.displayitem {
+  background-color: coral;
+}
 </style>
