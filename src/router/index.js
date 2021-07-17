34151a215e16579118a4cb8d230cae360fc295de
MartinYe1234 <martinye022@gmail.com>
2021-07-16 19:02:41 -0700

    second page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '/src/components/Home.vue'
+import PhotoUpload from '/src/components/PhotoUpload.vue'
 
 const routes = [
     {
-        path : '/',
+        path: '/',
         name: 'Home',
-        component : Home,
+        component: Home,
+    },
+    {
+        path: '/photoupload',
+        name: 'Photo Upload',
+        component: PhotoUpload,
     }
 ]
 const router = createRouter({
